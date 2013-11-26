87e4c285b39b970c4f59e89dcc0239d476eeb346
Christian Koptein <christian@koptein.de>
2013-11-26 20:51:08 +0100

    Initial release

---
+++ urly.rb
@@ -0,0 +1,42 @@
+require 'open-uri'
+require 'cinch'
+require 'pismo'
+
+# Automatically shorten URL's found in messages
+# Using the tinyURL API
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server   = "irc.freenode.net"
+    c.channels = ["#kopteintestchan","#kopteintestchan2"]
+    c.nick = "urly"
+  end
+
+  helpers do
+    def shorten(url)
+      if url.length < 60 
+        return nil
+      end
+      url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
+      url == "Error" ? nil : url
+    rescue OpenURI::HTTPError
+      nil
+    end
+  end
+
+  on :channel do |m|
+    urls = URI.extract(m.message, ["http","https"])
+
+    unless urls.empty?
+      short_urls = urls.map {|url| shorten(url) }.compact
+      doc = Pismo::Document.new(urls[0])
+      title = doc.title
+      unless short_urls.empty?
+        m.reply m.user.nick + ": " + short_urls.join(", ") + " Title: '" + title +"'"
+      end
+    end
+  end
+end
+
+bot.start
+
